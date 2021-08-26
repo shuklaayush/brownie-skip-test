ddaaf3a00727e7a479b2789abb58da79f6f81539
Ayush Shukla <shuklaayush247@gmail.com>
2021-08-26 13:12:43 +0530

    Add require network mark

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -1,9 +1,13 @@
 import pytest
 
-
-@pytest.mark.skip()
+@pytest.mark.skip(reason="No reason")
 def test_skip():
+    pytest.fail('hello')
     assert True
+
+@pytest.mark.require_network("dummy")
+def test_mark():
+    pass
 
 def test_noskip():
     assert True
